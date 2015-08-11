5695ea3aba7ba9497fb4f5828849b1106f7ec377
Pisyek Kumar <pisyek@gmail.com>
2015-08-11 23:34:20 +0800

    delete bak view file

--- application/views/blog/bak_footer.php
+++
@@ -1,3 +0,0 @@
-<div id="footer">
-    Copyright &copy; <?php echo mdate('%Y',now());?> <a href="http://www.pisyek.com/" target="_blank" rel="follow">Pisyek Kumar</a> &bull; All rights reserved
-</div><!-- Close footer -->
